69cffda9fe2da26eca5da0f7701a11bda8f04c59
heyitsjorge <jguzma60@calpoly.edu>
2024-10-02 11:32:37 -0700

    -implemented deleting name

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -39,18 +39,44 @@ const findUserByName = (name) => {
     );
 };
 
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
+const removeUserById = (id) =>
+    users["users_list"].delete((user) => user["id"] ===id);
+
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-        let result = findUserByName(name);
-        result = { users_list: result };
-        res.send(result);
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
     } else {
-        res.send(users);
+        res.send(result);
     }
 });
+
+app.delete("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    let result = removeUserById(id);
+    if (result === undefined){
+        res.status(404).send("Resource not found.");
+    } else{
+        res.send(result);
+    }
+})
 
 app.listen(port,() => {
     console.log(
